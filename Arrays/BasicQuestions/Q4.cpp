9c8eb4ce8b1e9a0ff0e161dadc36b20352502f1f
Isha Sharma <94511920+ishaosharma@users.noreply.github.com>
2022-04-05 12:50:23 +0530

    Create Q4.cpp

---
+++ Arrays/BasicQuestions/Q4.cpp
@@ -0,0 +1,16 @@
+Write a program to reverse the array.[Hint: use indexes] Here , 5 is the size of array and then elements of arrays are input
+
+        // int n;
+        // cout << "Enter size of array = " << endl;
+        // cin >> n;
+        // int arr[n];
+        // cout << "Enter elements in array: " << endl;
+
+        // for (int i = 0; i < n; i++){
+        //     cin >> arr[i];
+        // }
+
+        // for (int i = n-1; i >= 0; i--)
+        // {
+        //    cout << arr[i] << " ";
+        // }cout << endl;
