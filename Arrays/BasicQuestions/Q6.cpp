51f7255bfdb914ccbcf827f03fbd425debeb6758
Isha Sharma <94511920+ishaosharma@users.noreply.github.com>
2022-04-05 14:35:38 +0530

    Create Q6.cpp

---
+++ Arrays/BasicQuestions/Q6.cpp
@@ -0,0 +1,17 @@
+Find the Kth largest and Kth smallest number in an array.
+
+        // int n,k;
+        // cout << "Enter size of array = " << endl;
+        // cin >> n;
+        // cout << "Enter k = ";
+        // cin >> k;
+        // int arr[n];
+        // cout << "Enter elements in array: " << endl;
+
+        // for (int i = 0; i < n; i++){
+        //     cin >> arr[i];
+        // }
+        // sort(arr,arr+n);
+
+        //     cout << "Smallest = " << arr[k-1] << endl;
+        //     cout << "Largest = " << arr[n-k];
